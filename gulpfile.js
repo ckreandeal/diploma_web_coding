631994aab2ba6d62f1a1f0c47e2985848feb14f8
Ckrendel <danich1993@mail.ru>
2021-03-02 01:17:31 +0000

    gilp img + imports optimisation

--- gulpfile.js
+++ gulpfile.js
@@ -5,14 +5,14 @@ const rename = require("gulp-rename");
 const uglify = require('gulp-uglify-es').default;
 const imagemin = require('gulp-imagemin');
 
-const  js_script = () => {
+const  jsScript = () => {
     return src("js/script.js")
         .pipe(rename("script.min.js"))
         .pipe(uglify())
         .pipe(dest("build/js"));
 };
 
-const  js_slider = () => {
+const  jsSlider = () => {
   return src("js/slider.js")
       .pipe(rename("slider.min.js"))
       .pipe(uglify())
@@ -20,17 +20,16 @@ const  js_slider = () => {
 };
 
 const css = () => {
-  return src('css/styles.css')
+  return src('css/*.css')
     .pipe(csso())
     .pipe(dest('build/css'))
 };
 
-// TODO please check functions for images
-// const img = () => {
-//   return src('img/*')
-//     .pipe(imagemin())
-//     .pipe(dest('img/'))
-// };
+const img = () => {
+  return src('img/*')
+    .pipe(imagemin())
+    .pipe(dest('build/img'))
+};
 
 const startServer = () => {
   server.init({
@@ -49,4 +48,4 @@ const reload = (done) => {
 };
 
 exports.dev = startServer;
-exports.build = series(css, js_script, js_slider);
+exports.build = series(css, jsScript, jsSlider, img);
